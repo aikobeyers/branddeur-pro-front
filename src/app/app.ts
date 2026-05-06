439cdcd0bdbd86a8eafe1fc50efba1c26456e26c
aikobeyers <aikobeyers@hotmail.com>
2026-05-06 21:31:11 +0200

    Update title

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('branddeur-pro-front');
+  protected readonly title = signal('BrandDeur PRO');
 }
