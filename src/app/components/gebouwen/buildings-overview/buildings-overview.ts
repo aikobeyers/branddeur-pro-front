d520974643dbd9237ea98552b045fb72f6c36b10
aikobeyers <aikobeyers@hotmail.com>
2026-05-15 17:58:56 +0200

    Doors per building

--- src/app/components/gebouwen/buildings-overview/buildings-overview.ts
+++ src/app/components/gebouwen/buildings-overview/buildings-overview.ts
@@ -4,6 +4,23 @@ import { httpResource } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
 import { BuildingCreateModal } from '../building-create-modal/building-create-modal';
 import { Gebouw } from '../../../models/gebouw';
+import { Branddeur } from '../../../models/branddeur';
+
+const STATUS_LABELS: Record<string, string> = {
+  A: 'Goedgekeurd',
+  B: 'Herstel nodig',
+  C: 'Afgekeurd',
+};
+
+interface DoorStatusCounts {
+  total: number;
+  approved: number;
+  approvedLabel: string;
+  warning: number;
+  warningLabel: string;
+  error: number;
+  errorLabel: string;
+}
 
 interface GebouwCardViewModel {
   id: string;
@@ -12,6 +29,7 @@ interface GebouwCardViewModel {
   locations: string[];
   floorCount: number;
   locationCount: number;
+  doorStats: DoorStatusCounts;
 }
 
 @Component({
@@ -30,10 +48,28 @@ export class BuildingsOverview {
     method: 'GET'
   }));
 
-  protected readonly cards = computed<GebouwCardViewModel[]>(() =>
-    (this.gebouwenResource.value() ?? []).map(gebouw => {
+  protected readonly branddeurenResource = httpResource<Branddeur[]>(() => ({
+    url: `${environment.baseUrl}/branddeuren`,
+    method: 'GET'
+  }));
+
+  protected readonly cards = computed<GebouwCardViewModel[]>(() => {
+    const branddeuren = this.branddeurenResource.value() ?? [];
+
+    return (this.gebouwenResource.value() ?? []).map(gebouw => {
       const floors = this.cleanStringList(gebouw.floor);
       const locations = this.cleanStringList(gebouw.location);
+      const doorsInBuilding = branddeuren.filter(d => d.building === gebouw.name);
+
+      const doorStats: DoorStatusCounts = {
+        total: doorsInBuilding.length,
+        approved: doorsInBuilding.filter(d => d.mostRecentInspection?.inspectionResult?.statusCode === 'A').length,
+        approvedLabel: doorsInBuilding.find(d => d.mostRecentInspection?.inspectionResult?.statusCode === 'A')?.mostRecentInspection?.inspectionResult?.statusValue || STATUS_LABELS['A'],
+        warning: doorsInBuilding.filter(d => d.mostRecentInspection?.inspectionResult?.statusCode === 'B').length,
+        warningLabel: doorsInBuilding.find(d => d.mostRecentInspection?.inspectionResult?.statusCode === 'B')?.mostRecentInspection?.inspectionResult?.statusValue || STATUS_LABELS['B'],
+        error: doorsInBuilding.filter(d => d.mostRecentInspection?.inspectionResult?.statusCode === 'C').length,
+        errorLabel: doorsInBuilding.find(d => d.mostRecentInspection?.inspectionResult?.statusCode === 'C')?.mostRecentInspection?.inspectionResult?.statusValue || STATUS_LABELS['C'],
+      };
 
       return {
         id: gebouw._id,
@@ -41,10 +77,11 @@ export class BuildingsOverview {
         floors,
         locations,
         floorCount: floors.length,
-        locationCount: locations.length
+        locationCount: locations.length,
+        doorStats
       };
-    })
-  );
+    });
+  });
 
   public constructor() {
     effect(() => {
